178bfdff9d2eaa543ceb1554ffdb82c92570a38a
Weixuan XIAO <veyx.shaw@gmail.com>
2021-11-14 00:45:32 +0100

    Add index and station maui app for test

---
+++ kde/maui/index/index.py
@@ -0,0 +1,27 @@
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.targets['2.1.0'] = "https://download.kde.org/stable/maui/index/2.1.0/index-fm-2.1.0.tar.xz"
+
+        self.description = "mauikit Index file browser"
+        self.defaultTarget = '2.1.0'
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = None
+        self.runtimeDependencies["kde/maui/mauikit"] = None
+        self.runtimeDependencies["kde/maui/mauikitfilebrowsing"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/karchive"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **args):
+        CMakePackageBase.__init__(self)
